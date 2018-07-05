963f585dedb1164c1a975b622bd35b6c5d158dbb
Tylor Steinberger <tylor@tryfynd.com>
2018-07-04 22:05:07 -0400

    change the assertion

--- source/test.ts
+++ source/test.ts
@@ -5,5 +5,5 @@ export const suite = describe('Things', [
     it('failing', ({ ok }) => ok(false)), 
     it('passing', ({ ok }) => ok(true))
   ]),
-  it('with promise', ({ ok }) => Promise.resolve(true).then(ok))
+  it('with promise', ({ equal }) => Promise.resolve(true).then(equal(true)))
 ])
